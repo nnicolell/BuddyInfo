d69d341f604c782ee7394a9b284011878f8f0d2e
nicolelim <nicolelim@cmail.carleton.ca>
2022-09-19 00:33:49 -0400

    Added print statement in main

---
+++ src/AddressBook.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class AddressBook {
+    private ArrayList<BuddyInfo> buddyList;
+
+    private void addBuddy(){
+
+    }
+
+    private void removeBuddy(){
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Address Book");
+    }
+
+}
