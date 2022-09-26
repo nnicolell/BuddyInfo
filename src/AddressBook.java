1e9f5049acf967fa891e4de4091c241811cf0a90
nicolelim <nicolelim@cmail.carleton.ca>
2022-09-25 21:54:03 -0400

    Added the add and remove methods.

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -3,19 +3,28 @@ import java.util.*;
 public class AddressBook {
     private ArrayList<BuddyInfo> buddyList;
 
-    private void addBuddy(BuddyInfo buddy){
-
+    public AddressBook() {
+        buddyList = new ArrayList<>();
     }
 
-    private void removeBuddy(BuddyInfo buddy){
+    private void addBuddy(BuddyInfo buddy){
+        if (buddy != null){
+            buddyList.add(buddy);
+        }
+    }
 
+    private BuddyInfo removeBuddy(int index){
+        if (index >= 0 && index < buddyList.size()){
+            return buddyList.remove(index);
+        }
+        return null;
     }
 
     public static void main(String[] args) {
         BuddyInfo buddy = new BuddyInfo("Tom","Carleton","613");
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
-        addressBook.removeBuddy(buddy);
+        addressBook.removeBuddy(0);
     }
 
 }
